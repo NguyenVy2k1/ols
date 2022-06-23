a109ab6261de024da4f5078f0058e080939a4946
NguyenVy2k1 <103101604+NguyenVy2k1@users.noreply.github.com>
2022-06-23 14:37:46 +0700

    Update web.py

--- web.py
+++ web.py
@@ -6,8 +6,7 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("Breast_cancer.h5") #model m train
-
+model = tf.keras.models.load_model("https://drive.google.com/file/d/1UXDdOYHGylpczA5EVp2O50reDLzdlYH9/view?usp=sharing")
 selected = option_menu(None, ["Diagnostic", "More"], 
     icons=[ "upload", 'bookmark-fill'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
